34a636c011e18ace8b1e1c74c3296d6bba9ddc5b
Daniel Huber <dhuber@terpmail.umd.edu>
2025-02-26 19:25:58 +0000

    ex7 switch case

--- SwitchCase-ex7.cpp
+++ SwitchCase-ex7.cpp
@@ -1,1 +1,26 @@
 // add your code here
+#include <iostream>
+using namespace std;
+
+int main() { 
+    int N;
+    cout << "Please enter an integer value: ";
+    cin >> N;
+    int x = 2;
+    if (N % 2 == 0 & N < 10) {
+        x = 0;
+    }
+    if (N % 2 == 1) {
+        x = 1;
+    }
+    switch(x) {
+        case 0:
+            cout << N*N; 
+            break;
+        case 1:
+            cout << double(N)/2;
+            break;
+        default:
+            cout << "hahaha";
+    }
+}
