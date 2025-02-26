a4b9a757cf92813b87be9b7bfeaef0afb512ec0a
Daniel Huber <dhuber@terpmail.umd.edu>
2025-02-26 18:59:19 +0000

    ex5 cin

--- UserInput-ex5.cpp
+++ UserInput-ex5.cpp
@@ -1,1 +1,19 @@
-// Add your code here
+#include <iostream>
+using namespace std;
+
+int factorial (int n) {
+    int factor = 1;
+    if (n > 1) {
+        for (int j = 1; j <= n; ++j) {
+            factor = factor * j;
+        }
+    }
+    return factor;
+}
+
+int main() {
+    int N;
+    cout << "Please enter an integer value: ";
+    cin >> N;
+    cout << "The value you entered is " << N << " and its factorial is " << factorial(N) << "\n"; 
+}
